a3bc6c98690223ae493947209904bbde042f7b72
larionov-m-d-home <mihail.larionow2001@gmail.com>
2025-05-02 00:16:27 +0300

    feature: init swagger

--- internal/handler/http_handler.go
+++ internal/handler/http_handler.go
@@ -14,6 +14,16 @@ func CreateHandler() *HttpHandler {
 	return &HttpHandler{}
 }
 
+// Execute обрабатывает список инструкций
+// @Summary Выполняет инструкции calc и print
+// @Description Получает JSON с инструкциями, выполняет их и возвращает результат
+// @Tags Instructions
+// @Accept  json
+// @Produce  json
+// @Param   instructions body []service.Instruction true "Список инструкций"
+// @Success 200 {object} service.Response
+// @Failure 400 {object} map[string]string
+// @Router /execute [post]
 func (h *HttpHandler) Execute(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Only POST method allowed", http.StatusMethodNotAllowed)
